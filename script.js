69925be4430f8714cea9d48dd0f4c45737f34f4d
Abideen Olafimihan <olafimihana@gmail.com>
2022-11-06 20:52:30 +0000

    implementing smooth scrolling to the nav links (using event delegation)

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 'use strict';
 
-//MODAL WINDOW
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const openModalBtn = document.querySelectorAll('.btn--show-modal');
 const closeModalBtn = document.querySelector('.btn--close-modal');
+const section1 = document.querySelector('#section--1');
+const learnMoreBtn = document.querySelector('.btn--scroll-to');
+const navLink = document.querySelectorAll('.nav__link')
+const navLinks = document.querySelector('.nav__links')
 
 //handling opening of modal window
 const openModal = e => {
@@ -36,28 +39,42 @@ document.addEventListener('keydown', escapeCloseModal);
 const message = document.createElement('div');
 message.classList.add('cookie-message');
 message.innerHTML = `<p>We use cookies for improved functionalities and analytics.</p> <button class='btn btn--close--cookie'>Got it!</button>`;
-const html = `<p class='warn'>Warning:</p>`
+const html = `<p class='warn'>Warning:</p>`;
 
 //ADDING TO THE CREATED ELEMENT AS THE FIRST CHILD OF THE HEADER
-const header = document.querySelector('.header')
-header.append(message)
+const header = document.querySelector('.header');
+header.append(message);
 //ADD THE CREATED HTML AS THE FIRST CHILD OF THE CREATED MESSAGE ELEMENT
-message.insertAdjacentHTML('afterbegin', html)
+message.insertAdjacentHTML('afterbegin', html);
 
 //TO REMOVE THE COOKIE BY CLICKING THE BUTTON
-const closeCookieBtn = document.querySelector('.btn--close--cookie')
+const closeCookieBtn = document.querySelector('.btn--close--cookie');
 
-closeCookieBtn.addEventListener('click', function() {
-    message.remove()
-})
+closeCookieBtn.addEventListener('click', function () {
+  message.remove();
+});
 
 //IMPLEMENTING SMOOTH SCROLL TO SECTION 1
-const section1 = document.querySelector('#section--1')
-const learnMoreBtn = document.querySelector('.btn--scroll-to')
+learnMoreBtn.addEventListener('click', function () {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
 
-learnMoreBtn.addEventListener('click', function(){
-    section1.scrollIntoView({behavior: 'smooth'})
+//implementing smooth scroll using the navlinks
+/*
+navLink.forEach((link) => {
+  link.addEventListener('click', function(e){
+    e.preventDefault()
+    const id = this.getAttribute('href')
+    document.querySelector(id).scrollIntoView({behavior: 'smooth'})
+  })
 })
+*/
 
-
-
+//implementing smooth scrolling using the parent of nav-link via event delegation
+navLinks.addEventListener('click', function(e) {
+  e.preventDefault()
+  if(e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href')
+    document.querySelector(id).scrollIntoView({behavior: 'smooth'})
+  }
+})
